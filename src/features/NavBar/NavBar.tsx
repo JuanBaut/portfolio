33eb1a63c438cec278d63e7f682b6184cc3db387
JuanBaut <deserd@protonmail.com>
2024-04-03 21:12:30 -0500

    NavBar works

--- src/features/NavBar/NavBar.tsx
+++ src/features/NavBar/NavBar.tsx
@@ -1,38 +1,33 @@
-import { Link, useNavigate } from "react-router-dom";
 import {
   NavigationMenu,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   return (
     <div className="mx-auto w-[50%]">
       <NavigationMenu className="mx-auto my-4">
         <NavigationMenuList>
-
-          <NavigationMenuItem>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                about
-              </NavigationMenuLink>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem >
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                projects
-              </NavigationMenuLink>
-          </NavigationMenuItem>
-          
-          <Link to={"/photos"}>
-          <NavigationMenuItem>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                photos
-              </NavigationMenuLink>
-          </NavigationMenuItem>
+          <Link to="/">
+            <NavigationMenuItem className={navigationMenuTriggerStyle()}>
+              about
+            </NavigationMenuItem>
           </Link>
 
+          <Link to="/projects">
+            <NavigationMenuItem className={navigationMenuTriggerStyle()}>
+              projects
+            </NavigationMenuItem>
+          </Link>
+
+          <Link to="/photos">
+            <NavigationMenuItem className={navigationMenuTriggerStyle()}>
+              photos
+            </NavigationMenuItem>
+          </Link>
         </NavigationMenuList>
       </NavigationMenu>
     </div>
