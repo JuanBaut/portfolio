6992d1fcc2844a554f85aadc2ecd2d46b84d974d
JuanBaut <deserd@protonmail.com>
2024-04-05 18:27:32 -0500

    Projects start

--- src/features/Projects/Projects.tsx
+++ src/features/Projects/Projects.tsx
@@ -1,6 +1,6 @@
 const Projects: React.FC = () => {
   return (
-    <div className="mx-auto w-[50%] border border-accent">
+    <div className="mx-auto w-[50%]">
       <p>these are the Projects </p>
     </div>
   );
