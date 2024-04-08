5f66a75d57ba0f9539deab2e98a0d4bac697f570
JuanBaut <deserd@protonmail.com>
2024-04-08 00:14:51 -0500

    project page structure

--- src/features/Projects/Projects.tsx
+++ src/features/Projects/Projects.tsx
@@ -1,7 +1,20 @@
 const Projects: React.FC = () => {
   return (
-    <div className="mx-auto w-[50%]">
-      <p>these are the Projects </p>
+    <div className="mx-auto w-[90%] px-2 sm:w-[640px] md:w-[640px] lg:w-[640px] xl:w-[640px] 2xl:w-[640px]">
+      <div className="border-primary py-3">
+        <p className="font-bold">AppGuard</p>
+        <p className="text-sm">Descriptive text</p>
+      </div>
+      <hr />
+      <div className="border-primary py-3">
+        <p className="font-bold">Videogames App</p>
+        <p className="text-sm">Descriptive text</p>
+      </div>
+      <hr />
+      <div className="border-primary py-3">
+        <p className="font-bold">dotfiles</p>
+        <p className="text-sm">Descriptive text</p>
+      </div>
     </div>
   );
 };
