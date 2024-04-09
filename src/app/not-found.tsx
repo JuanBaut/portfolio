0eef787b02c4049566ca9869e404dbc6abcc701e
JuanBaut <deserd@protonmail.com>
2024-04-09 11:31:57 -0500

    new routes for about, projects and photos

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Undo2 } from "lucide-react";
+
+export default function NotFound() {
+  return (
+    <div className="mx-auto w-[90%] px-2 sm:w-[640px] md:w-[640px] lg:w-[640px] xl:w-[640px] 2xl:w-[640px]">
+      <div className="mx-auto my-[10%] w-[50%] text-center flex flex-col">
+        <p>
+          This place does not exist. <i>404</i>
+        </p>
+        <Button variant={"ghost"} className="mx-auto mt-4 h-7 p-0 px-2">
+          <p className="pr-2">go back</p>
+          <Undo2 className="m-0 size-5 p-0" />
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/photos/page.tsx
@@ -0,0 +1,8 @@
+export default function Photos() {
+  return (
+    <div className="mx-auto w-[90%] px-2 sm:w-[640px] md:w-[640px] lg:w-[640px] xl:w-[640px] 2xl:w-[640px]">
+      <p>Photos taken with my Nikon D3400 for fun.</p>
+      <div className="flex flex-wrap gap-2"></div>
+    </div>
+  );
+}
