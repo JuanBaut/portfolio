e69b91f4ec1362b304b165aa2462ba7efd598d21
JuanBaut <deserd@protonmail.com>
2024-04-22 12:41:37 -0500

    finished refactor of projects

--- src/app/projects/_dotfiles.tsx
+++ src/app/projects/_dotfiles.tsx
@@ -1,30 +1,37 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardTitle } from "@/components/ui/card";
-import { SquareArrowOutUpRight } from "lucide-react";
+import { Code } from "lucide-react";
 
 export default function Dotfiles() {
   return (
     <Card className="mb-4 border-primary p-2">
       <div className="flex flex-col gap-2">
-        <CardTitle>
+        <CardTitle className="flex items-center gap-2">
+          <p className="mr-auto">Dotfiles</p>
           <a
             href="https://github.com/JuanBaut/dotfiles"
             target="_blank"
             rel="noreferrer"
-            className="flex font-bold "
+            className="inline-flex"
           >
-            Dotfiles
-            <SquareArrowOutUpRight className="my-auto ml-2 size-4" />
+            <Button size={"xs"} variant={"special_sm"}>
+              Code
+              <Code className="ml-2 size-4" />
+            </Button>
           </a>
         </CardTitle>
-        <div className="flex flex-wrap gap-2">
-          <Badge>Neovim</Badge>
-          <Badge>Shell</Badge>
-        </div>
+        <hr />
       </div>
+
       <p className="pt-2 text-sm text-muted">
         My personal configuration files used in my dev environment.
       </p>
+
+      <div className="flex flex-wrap justify-center gap-2 px-[10%] pt-2">
+        <Badge>Neovim</Badge>
+        <Badge>Shell</Badge>
+      </div>
     </Card>
   );
 }
--- src/app/projects/_portfolio.tsx
+++ src/app/projects/_portfolio.tsx
@@ -1,32 +1,40 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardTitle } from "@/components/ui/card";
-import { SquareArrowOutUpRight } from "lucide-react";
+import { Code } from "lucide-react";
 
 export default function Portfolio() {
   return (
     <Card className="mb-4 border-primary p-2">
       <div className="flex flex-col gap-2">
-        <CardTitle>
+        <CardTitle className="flex items-center gap-2">
+          <p className="mr-auto">Portfolio</p>
           <a
             href="https://github.com/JuanBaut/portfolio"
             target="_blank"
             rel="noreferrer"
-            className="flex font-bold "
+            className="inline-flex"
           >
-            Portfolio
-            <SquareArrowOutUpRight className="my-auto ml-2 size-4" />
+            <Button size={"xs"} variant={"special_sm"}>
+              Code
+              <Code className="ml-2 size-4" />
+            </Button>
           </a>
         </CardTitle>
-        <div className="flex flex-wrap gap-2">
-          <Badge>TS</Badge>
-          <Badge>Next.js</Badge>
-          <Badge>Tailwind</Badge>
-          <Badge>Shadcn/ui</Badge>
-        </div>
+        <hr />
       </div>
+
       <p className="pt-2 text-sm text-muted">
         The website you are currently looking at.
       </p>
+
+      <div className="flex flex-wrap justify-center gap-2 px-[10%] pt-2">
+        <Badge>TS</Badge>
+        <Badge>Next.js</Badge>
+        <Badge>Tailwind</Badge>
+        <Badge>Shadcn/ui</Badge>
+        <Badge>Framer motion</Badge>
+      </div>
     </Card>
   );
 }
