29fccf3f8a089c0aae8fbfe84040db0a1c2962ff
JuanBaut <deserd@protonmail.com>
2024-04-22 12:52:10 -0500

    better photos text TODO: better grid

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -2,8 +2,11 @@ import Images from "@/app/photos/_images";
 
 export default async function Photos() {
   return (
-    <div className="mx-auto w-full px-2">
-      <p className="text-center">Photos taken with my Nikon D3400 for fun.</p>
+    <div>
+      <p className="mx-auto w-[95%] px-4 text-justify min-[640px]:w-[640px]">
+        I enjoy taking photos (specially car pics) as a hobbie, these are taken
+        with my Nikon D3400 for fun.
+      </p>
       <div className="py-4">
         <Images />
       </div>
