879993b9e4c32ab86f9c448263f17b2630ecce1d
JuanBaut <deserd@protonmail.com>
2024-07-06 16:26:13 -0500

    feat: changed theme button to mousedown

--- src/components/header/theme-toggle.tsx
+++ src/components/header/theme-toggle.tsx
@@ -31,7 +31,7 @@ export function ThemeToggle() {
       variant="ghost"
       size="icon"
       className="size-8"
-      onClick={handleChangeTheme}
+      onMouseDown={handleChangeTheme}
     >
       <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
