a26328d365fbf5dd07899bfa774264520a296f50
JuanBaut <deserd@protonmail.com>
2025-02-08 20:35:34 -0500

    HUGE file structure change and i18n implemented

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,15 @@
 import { MetadataRoute } from "next";
-import { getBlogPosts } from "../lib/posts";
 import { metaData } from "../config";
+import { getBlogPosts } from "../lib/posts";
 
 const BaseUrl = metaData.baseUrl.endsWith("/")
   ? metaData.baseUrl
   : `${metaData.baseUrl}/`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  let blogs = getBlogPosts().map((post) => ({
+  const blogs = await getBlogPosts();
+
+  let data = blogs.map((post) => ({
     url: `${BaseUrl}blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
@@ -17,5 +19,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes, ...data];
 }
